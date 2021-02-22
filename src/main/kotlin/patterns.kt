24f427caddbd32e4cf90d68b41bb17085ee6fe44
Robert Grimm <rmgrimm@users.noreply.github.com>
2021-02-22 02:38:00 -0600

    Yet more words and patterns

--- src/main/kotlin/patterns.kt
+++ src/main/kotlin/patterns.kt
@@ -1,11 +1,17 @@
 val patterns = arrayOf(
+    "{s} {s}",
     "{s} {p}",
     "{s} of {p}",
     "{p} of the {s}",
     "{s}'s {s}",
     "{s}: The Last {s}",
+    "{s}: Lost {p}",
     "{s}: {s} {p}",
+    "{p}: {s} {p}",
     "{s} of {p}: {s} and {s}",
     "{p} of {s}: The {s} {p}",
-    "{s} {s}: {s} of the {s}"
+    "{s} {s}: {s} of the {s}",
+    "{s} {s}: {s} of {p}",
+    "{s} {p}: {p} of the {s}",
+    "{s} {p}: {p} of {p}"
 )

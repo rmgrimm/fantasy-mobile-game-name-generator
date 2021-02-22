830a5ffea552abc832bd08e05d462a9fc1dae566
Robert Grimm <rmgrimm@users.noreply.github.com>
2021-02-22 01:28:47 -0600

    Add some words, adjust the generation patterns

--- src/main/kotlin/patterns.kt
+++ src/main/kotlin/patterns.kt
@@ -1,9 +1,10 @@
 val patterns = arrayOf(
     "{s} {p}",
     "{s} of {p}",
+    "{p} of the {s}",
     "{s}'s {s}",
     "{s}: {s} {p}",
     "{s} of {p}: {s} and {s}",
     "{p} of {s}: The {s} {p}",
-    "{s} {s}: {s} of {s}"
+    "{s} {s}: {s} of the {s}"
 )
